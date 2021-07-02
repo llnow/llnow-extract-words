fab506ce2910129bd7aeb47b6c154c0004278339
sy-app <seijuslp4@icloud.com>
2021-07-03 01:20:36 +0900

    s3に単語リストをtsvファイルとしてアップロード

--- hello_world/put_words.py
+++ hello_world/put_words.py
@@ -1,2 +1,8 @@
-def put_words(words):
-    pass
+import boto3
+
+
+def put_words(words, bucket):
+    s3 = boto3.resource('s3')
+    # s3に単語リストをtsvファイルとしてアップロード
+    file_path = 'tmp/words.tsv'
+    res = s3.Object(bucket, file_path).put(Body=' '.join(words))
