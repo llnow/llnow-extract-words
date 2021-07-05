e7e7e45724752e9c7f56b0fc6b973b805b4d8680
sy-app <seijuslp4@icloud.com>
2021-07-06 00:47:45 +0900

    ラブライブ！シリーズの基本的な単語をDynamoDBから取得してテキストから除去

--- hello_world/extract_texts.py
+++ hello_world/extract_texts.py
@@ -1,24 +1,27 @@
 import re
+import boto3
 
 
 def extract_texts(tweets):
+    # ラブライブ！シリーズの基本的な単語をDynamoDBから取得
+    table = boto3.resource('dynamodb').Table('lovelive_words')
+    tables = table.scan()
+    remove_words_pattern = '|'.join(tables['Items'][0]['words'])
+
     texts = []
-    remove_words = ['lovelive', 'LoveLive', 'ラブライブ', 'Aqours', 'aqours', 'サンシャイン', '沼津', 'sunshine', '虹ヶ咲',
-                    '虹ヶ咲学園スクールアイドル同好会', '同好会', 'Liella', 'スーパースター', 'ラブライバー', 'スクールアイドル', 'LoveLivestaff', 'スクフェス',
-                    'スクスタ']
     for tweet in tweets:
         text = tweet['text']
         # 'RT 'で始まるツイートを除外
         # if text.startswith('RT '):
         #     continue
         # URLを除去
-        text = re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-…]+', '', text)
+        text = re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-…]+', ' ', text)
         # 改行を除去
         # text=re.sub('\n', ' ', text)
         # 絵文字などを除去
-        text = re.sub(r'[^、。!?ー〜1-9a-zA-Zぁ-んァ-ヶ亜-腕纊-黑一-鿕]', '', text)
-        for rm_word in remove_words:
-            text = re.sub(rm_word, ' ', text)
+        text = re.sub(r'[^、。!?ー〜1-9a-zA-Zぁ-んァ-ヶ亜-腕纊-黑一-鿕]', ' ', text)
+        # ラブライブ！シリーズの基本的な単語を除去
+        text = re.sub(remove_words_pattern, ' ', text)
 
         texts.append(text)
 
