9a6c33d32e2a9b5237090b38eba787b61c8eefd0
sy-app <seijuslp4@icloud.com>
2021-07-08 02:19:42 +0900

    tweets_featureをdynamodbに記録するようにする

--- hello_world/extract_tweets_features.py
+++ hello_world/extract_tweets_features.py
@@ -1,17 +1,26 @@
 from posted_time_utc2jst import *
-import json
+import boto3
 
 
 def extract_tweets_features(tweets):
     n_tweet = len(tweets)
     latest_tweet_created_at = posted_time_utc2jst(tweets[0]['created_at'])
     oldest_tweet_created_at = posted_time_utc2jst(tweets[-1]['created_at'])
-    tweets_features = {}
-    tweets_features['n_tweet'] = n_tweet
-    tweets_features['latest_tweet_created_at'] = latest_tweet_created_at
-    tweets_features['oldest_tweet_created_at'] = oldest_tweet_created_at
 
-    # 取得したツイートの特徴をjsonファイルに出力
-    json_path = '/tmp/tweets_features.json'
-    with open(json_path, 'w') as f:
-        json.dump(tweets_features, f, indent=4, ensure_ascii=False)
+    # tweets_featureをdynamodbに記録
+    table = boto3.resource('dynamodb').Table('ll_now')
+    try:
+        table.update_item(
+            Key={'primary': 'tweets_feature'},
+            UpdateExpression="set feature.n_tweet=:nt, feature.latest_tweet_created_at=:ltca, "
+                             "feature.oldest_tweet_created_at=:otca",
+            ExpressionAttributeValues={
+                ':nt': n_tweet,
+                ':ltca': latest_tweet_created_at,
+                ':otca': oldest_tweet_created_at
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+    except Exception as e:
+        print(e)
