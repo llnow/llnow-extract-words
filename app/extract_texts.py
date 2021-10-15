4889ec1d5d0596bda3cf01e31668a530c7ceaf28
sy-app <seijuslp4@icloud.com>
2021-10-16 03:06:46 +0900

    "・"とギリシャ文字を除去しない

--- app/extract_texts.py
+++ app/extract_texts.py
@@ -26,7 +26,7 @@ def extract_texts(tweets):
         # 改行を除去
         # text=re.sub('\n', ' ', text)
         # 絵文字などを除去
-        text = re.sub(r'[^、。!?ー〜1-9a-zA-Zぁ-んァ-ヶ亜-腕纊-黑一-鿕]', ' ', text)
+        text = re.sub(r'[^、。!?ー〜1-9a-zA-Zぁ-んァ-ヶ亜-腕纊-黑一-鿕・Α-ω]', ' ', text)
         # 不要な単語を除去
         text = re.sub(remove_words_pattern, '', text)
 
