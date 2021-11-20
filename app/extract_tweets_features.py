ecb19f4f141f6f8f46144ee2338fe0e43594fdae
sy-app <seijuslp4@icloud.com>
2021-11-21 04:59:39 +0900

    tweets_featuresをDynamoDBではなくssmパラメータストアで管理する

--- app/extract_tweets_features.py
+++ app/extract_tweets_features.py
@@ -1,26 +1,30 @@
 from posted_time_utc2jst import *
+import os
 import boto3
+import json
+
+region = os.environ['AWS_DEFAULT_REGION']
+ssm = boto3.client('ssm', region)
 
 
-def extract_tweets_features(tweets):
+def extract_tweets_features(tweets, mode):
     n_tweet = len(tweets)
     latest_tweet_created_at = posted_time_utc2jst(tweets[0]['created_at'])
     oldest_tweet_created_at = posted_time_utc2jst(tweets[-1]['created_at'])
+    tweets_features = json.dumps({
+        "latest_tweet_created_at": latest_tweet_created_at,
+        "n_tweet": n_tweet,
+        "oldest_tweet_created_at": oldest_tweet_created_at
+    })
 
-    # tweets_featureをdynamodbに記録
-    table = boto3.resource('dynamodb').Table('ll_now')
-    try:
-        table.update_item(
-            Key={'primary': 'tweets_feature'},
-            UpdateExpression="set feature.n_tweet=:nt, feature.latest_tweet_created_at=:ltca, "
-                             "feature.oldest_tweet_created_at=:otca",
-            ExpressionAttributeValues={
-                ':nt': n_tweet,
-                ':ltca': latest_tweet_created_at,
-                ':otca': oldest_tweet_created_at
-            },
-            ReturnValues="UPDATED_NEW"
-        )
+    # tweets_featureをSystem Managerパラメータストアに記録
+    key = 'll-now-tweets-features-{}'.format(mode)
+    update_ssm_param(key, tweets_features)
 
-    except Exception as e:
-        print(e)
+
+def update_ssm_param(key, value):
+    ssm.put_parameter(
+        Name=key,
+        Value=value,
+        Overwrite=True
+    )
