bae0eb692b38001155f4dc537c9dd33e9fca5e67
sy-app <59391585+sy-app@users.noreply.github.com>
2021-11-21 05:01:43 +0900

    Merge pull request #11
    * tweets_featuresをDynamoDBではなくssmパラメータストアで管理する

--- app/main.py
+++ app/main.py
@@ -8,9 +8,10 @@ from put_words import *
 def main(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
+    mode = context.invoked_function_arn.split(':')[-1]
 
     tweets = fetch_tweets(bucket, key)
-    extract_tweets_features(tweets)
+    extract_tweets_features(tweets, mode)
     texts = extract_texts(tweets)
     words = split_texts_into_words(texts, bucket)
     put_words(words, bucket)
