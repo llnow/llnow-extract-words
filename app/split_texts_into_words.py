de9c22523fca4d8844326431709b5579dd22c8d4
sy-app <seijuslp4@icloud.com>
2021-11-21 07:08:58 +0900

    modeによらずユーザ辞書を本番用バケットから取得

--- app/split_texts_into_words.py
+++ app/split_texts_into_words.py
@@ -12,7 +12,7 @@ def split_texts_into_words(texts, bucket):
 
     # ユーザ辞書をs3からダウンロード
     s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket)
+    bucket = s3.Bucket('ll-now-material')
     file_key = 'lovelive_word_dic.csv'
     file_path = '/tmp/lovelive_word_dic.csv'
     bucket.download_file(file_key, file_path)
