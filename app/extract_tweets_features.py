c18c75f9e386af2f97538a017b72ebfc79ca18b1
sy-app <59391585+sy-app@users.noreply.github.com>
2021-12-07 19:41:10 +0900

    tweets_features.jsonのフォーマットを修正 (#21)

--- app/extract_tweets_features.py
+++ app/extract_tweets_features.py
@@ -7,12 +7,12 @@ def extract_tweets_features(tweets, search_metadata, bucket):
     n_tweet = len(tweets)
     latest_tweet_created_at = posted_time_utc2jst(tweets[0]['created_at'])
     oldest_tweet_created_at = posted_time_utc2jst(tweets[-1]['created_at'])
-    tweets_features = json.dumps({
+    tweets_features = {
         "latest_tweet_created_at": latest_tweet_created_at,
         "n_tweet": n_tweet,
         "oldest_tweet_created_at": oldest_tweet_created_at,
         'search_metadata': search_metadata
-    })
+    }
 
     path = '/tmp/tweets_features.json'
     with open(path, 'w') as f:
